b736c465b19624267958092962f420e1063d862a
richard tsang <richard.tsang9641@gmail.com>
2024-03-27 22:27:10 +0800

    update v

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
               height={24}
               priority
             />
-            testing
+            testing...
           </a>
         </div>
       </div>
